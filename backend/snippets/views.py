4e2b26e0a9156b72aacbcb7e78ca3987674491a6
Lordkodo <mabillot.paul@gmail.com>
2017-08-06 00:00:16 +0200

    clean up

--- backend/snippets/views.py
+++ backend/snippets/views.py
@@ -4,6 +4,9 @@ from rest_framework import status
 import hashlib
 import subprocess
 
+RESULT_FILE = 'result.csv'
+BUCKET = 'gs://lordkodo-dropper/drop/'
+
 @api_view(['POST'])
 def filetransform(request, format=None):
     #Test extension
@@ -22,12 +25,15 @@ def filetransform(request, format=None):
         'url': generateURL(md5),
     })
 
+# Calcule the MD5
 def calcMD5(data):
     return hashlib.md5(data.encode('utf-8')).hexdigest()
 
+# Return the url to the result file
 def generateURL(md5):
-    return 'https://storage.googleapis.com/lordkodo-dropper/drop/' + md5 + '/dst.csv'
+    return 'https://storage.googleapis.com/lordkodo-dropper/drop/' + md5 + '/result.csv'
 
+# Return True if extension is wrong
 def wrongExtension(name):
     if len(name.split('.')) < 2:
         return True
@@ -35,9 +41,11 @@ def wrongExtension(name):
     extension = name.split('.')[1]
     return extension != 'csv' and extension != 'xml'
 
+# Export file on GCS
 def exportGCS(name, md5):
-    subprocess.Popen(['gsutil','cp', 'dst.csv', 'gs://lordkodo-dropper/drop/' + md5 + '/' + name])
+    subprocess.Popen(['gsutil','cp', name, BUCKET + md5 + '/' + name])
 
+# Create a file and save it in GCS
 def saveFile(name, data, md5):
     f = open(name,"w+")
     f.write(data)
@@ -45,12 +53,14 @@ def saveFile(name, data, md5):
     # save on gcs
     exportGCS(name, md5)
 
+# Exec python script and save it on GCS
 def execAndSave(md5):
     subprocess.Popen(['python','-u', 'app/static/process.py'])
 
     #save on gcs
-    exportGCS('dst.csv', md5)
-    subprocess.Popen(['gsutil','acl', 'ch', '-u', 'AllUsers:R', 'gs://lordkodo-dropper/drop/' + md5 + '/dst.csv'])
+    exportGCS(RESULT_FILE, md5)
+    subprocess.Popen(['gsutil','acl', 'ch', '-u', 'AllUsers:R', BUCKET + md5 + '/result.csv'])
 
+# Clean the workspace
 def clean(name):
-    subprocess.Popen(['rm','-f', name, 'dst.csv'])
+    subprocess.Popen(['rm','-f', name, RESULT_FILE])
