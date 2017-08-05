a843408e23a7dc019532480d83fed23cc4d7ea97
Lordkodo <mabillot.paul@gmail.com>
2017-08-05 10:47:00 +0200

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dropper Front & Back
+
+* That's a service for dropping file
+* You got frontend, who test and send file to the backend
+* And back who test too, process and save result in GCS
--- front/scripts/less.js
+++ front/scripts/less.js
@@ -2,29 +2,6 @@ const Path = require('path');
 const fs = require('fs-extra');
 const childProcess = require('child_process');
 
-// function translate(path) {
-// 	let stat;
-// 	let file;
-//
-// 	fs.readdirSync(path).map((file) => {
-// 		file = Path.resolve(path + '/' + file);
-// 		if (fs.lstatSync(file).isDirectory()) {
-// 			translate(file);
-// 		} else if (Path.basename(file).indexOf('.less') !== -1) {
-// 			const newName = Path.basename(file).split('.')[0] + '.css';
-// 			const newPath = Path.resolve(path, newName);
-//
-// 			childProcess.exec('lessc ' + file + ' ' + newPath, (error, stdout, stderr) => {
-// 				if (error) {
-// 					console.log('Less script error: ', error);
-// 				}
-// 			});
-// 		}
-// 	})
-// }
-//
-// translate(Path.resolve(__dirname + '/../src/'));
-
 const chokidar = require('chokidar');
 
 function translate(path) {
