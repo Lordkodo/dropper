575bdb9a94dd06201c88350ac58fdf5164c98925
Paul Mabillot <mabillot.paul@gmail.com>
2017-08-07 19:24:39 +0200

    Update startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
+# Clean previous version
 rm -rf dropper
+
+# Download latest version
 git clone https://github.com/lordkodo/dropper.git
+
+# Run it !
 cd dropper/backend
 python3.5 manage.py runserver 0.0.0:8000
